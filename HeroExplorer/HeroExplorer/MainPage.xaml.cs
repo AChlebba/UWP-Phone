a495e2b8e12c8e888a9b69ebf1e5122938476a7d
AChlebba <adam343@op.pl>
2016-05-08 15:05:55 +0200

    Now shows comic details

--- HeroExplorer/HeroExplorer/MainPage.xaml.cs
+++ HeroExplorer/HeroExplorer/MainPage.xaml.cs
@@ -59,6 +59,11 @@ namespace HeroExplorer
             MyProgressRing.IsActive = true;
             MyProgressRing.Visibility = Visibility.Visible;
 
+            ComicDetailNameTextBlock.Text = "";
+            ComicDetailDescriptionTextBlock.Text = "";
+            ComicBuyTextBlock.Text = "";
+            ComicDetailImage.Source = null;
+
             var selectedCharacter = (Character)e.ClickedItem;
 
             DetailNameTextBlock.Text = selectedCharacter.name;
@@ -77,7 +82,47 @@ namespace HeroExplorer
             MyProgressRing.Visibility = Visibility.Collapsed;
         }
 
-        private async void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        private void ComicsGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedComic = (ComicBook)e.ClickedItem;
+
+            ComicDetailNameTextBlock.Text = selectedComic.title;
+
+            if (selectedComic.description != null)
+                ComicDetailDescriptionTextBlock.Text = selectedComic.description;
+
+            try
+            {
+                ComicBuyTextBlock.Text = selectedComic.urls[1].url;
+            }
+
+            catch (Exception)
+            {
+                return;
+            }
+
+            var largeImage = new BitmapImage();
+            Uri uri = new Uri(selectedComic.thumbnail.large, UriKind.Absolute);
+            largeImage.UriSource = uri;
+            ComicDetailImage.Source = largeImage;
+        }
+
+        private async void HyperlinkButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var selectedLink = ComicBuyTextBlock.Text;
+
+            string uriToLaunch = @selectedLink;
+
+            var uri = new Uri(uriToLaunch);
+
+            var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+        }
+
+
+
+
+
+        /*private async void GridView_ItemClick(object sender, ItemClickEventArgs e)
         {
             var selectedComic = (ComicBook)e.ClickedItem;
 
@@ -97,7 +142,8 @@ namespace HeroExplorer
             {
                 return;
             }
-        }
+        }*/
+
 
     }
     
